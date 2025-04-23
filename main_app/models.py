f4025791811287ede71a26b94bf5b60b6fdb66e1
Matt Mendez Zugenbuehler <mzugenbuehler@gmail.com>
2025-04-22 19:13:25 -0700

    updated routes and app functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    # Define a method to get the URL for this particular cat instance
+    def get_absolute_url(self):
+        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
+from django.shortcuts import render
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-# Create your views here.
+def contact(request):
+    return render(request, 'contact.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
